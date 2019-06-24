6dd2c22082d4bbd797fad37647d4e2a9afd4c767
dmanasa6 <34191637+dmanasa6@users.noreply.github.com>
2019-06-24 18:32:02 +0530

    Updated amazon_search_url

--- recommendation/scraper/amazon_scraper.py
+++ recommendation/scraper/amazon_scraper.py
@@ -3,8 +3,8 @@ import unicodecsv as csv
 from lxml import html
 
 def parse(product):
-
-    url = 'https://www.amazon.in/s?k={0}&rh=n%3A1389401031&ref=nb_sb_noss'.format(product)
+    # url for scraping details of product from amazon
+    url = 'https://www.amazon.in/s?k={0}&ref=nb_sb_noss'.format(product)       
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
     failed = False
 
@@ -31,9 +31,9 @@ def parse(product):
 
     for product in product_listings:
         raw_name = product.xpath('.//span[contains(@class,"a-text-normal")]//text()')        # product name
-        raw_url = product.xpath('.//a[contains(@class,"a-text-normal")]/@href')
-        raw_price = product.xpath('.//span[@class="a-price"]//text()')  #price
-        raw_image = product.xpath('.//img[contains(@class,"s-image")]/@src')       # product_image
+        raw_url = product.xpath('.//a[contains(@class,"a-text-normal")]/@href')              # product url
+        raw_price = product.xpath('.//span[@class="a-price"]//text()')                       # product_price
+        raw_image = product.xpath('.//img[contains(@class,"s-image")]/@src')                 # product_image
         #raw_rating = product.xpath('//span[@class="a-icon-alt"]//text()')
         name  = ' '.join(' '.join(raw_name).split())
         image = ' '.join(' '.join(raw_image).split())
@@ -58,13 +58,7 @@ if __name__=="__main__":
 
     scraped_data =  parse(product)
     if scraped_data:
-        print ("Writing scraped data to amazon-scraped-data.csv")
-        with open('amazon-scraped-data.csv','wb') as csvfile:
-            fieldnames = ["name","price","image","url","website"]
-            writer = csv.DictWriter(csvfile,fieldnames = fieldnames,quoting=csv.QUOTE_ALL)
-            writer.writeheader()
-            for data in scraped_data:
-                writer.writerow(data)
+        print ("---- Data is scraped ----")
     else:
         print("No data scraped")
 
