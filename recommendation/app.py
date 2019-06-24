9162ee91bebe75328e4a7c8c85320c5a50d84c36
dmanasa6 <34191637+dmanasa6@users.noreply.github.com>
2019-06-24 18:33:42 +0530

    Add files via upload

--- recommendation/app.py
+++ recommendation/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request , redirect
 import xlrd
 import requests
 from lxml import html
 import unicodecsv as csv
+import amazon_scraper
+import snapdeal_scraper
 
 app = Flask(__name__)
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
-failed = False
 
 workbook = xlrd.open_workbook('Dataset_for_Gifts.xlsx') 	# import values from the dataset
 			
@@ -61,68 +60,36 @@ def home():
 	return render_template("category.html",gender = gender, category = category,masterset = masterset)
 
 
-@app.route('/category?=<product_category>/gender?=<gender>')
+@app.route('/product?=<product_category>/gender?=<gender>')
 def view_category(product_category,gender):
 	if(product_category in category_list):
 		if(product_category in category_list[2::]):
 			product_category = gender + " " + product_category
-	return render_template("view_product.html",masterset = masterset, product_category = product_category)
+		return render_template("view_product.html",masterset = masterset, product_category = product_category)
+	else:
+		return redirect(url_for('view_product',product = product_category))
 
 
 @app.route('/product?=<product>')
 def view_product(product):
-	url = 'https://www.amazon.in/s?k={0}&rh=n%3A1389401031&ref=nb_sb_noss'.format(product)
-	scraped_products = []
+	
+	amazon_scraped_products = amazon_scraper.parse(product)
+	snapdeal_scraped_products = snapdeal_scraper.parse(product)
+	return render_template("products.html",amazon_scraped_products = amazon_scraped_products,snapdeal_scraped_products = snapdeal_scraped_products)
 
-	print ("Retrieving %s"%(url))
-	response = requests.get(url, headers=headers, verify=False)
-	parser = html.fromstring(response.text)
-	print ("Parsing page")
 
-	product_listings = parser.xpath('//div[contains(@class,"s-result-item")]')
 
-	print product_listings
-
-	for product in product_listings:
-		raw_name = product.xpath('.//span[contains(@class,"a-text-normal")]//text()')        # product name
-		raw_url = product.xpath('.//a[contains(@class,"a-text-normal")]/@href')
-		raw_price = product.xpath('.//span[@class="a-price"]//text()')  #price
-        raw_image = product.xpath('.//img[contains(@class,"s-image")]/@src')       # product_image
-        #raw_rating = product.xpath('//span[@class="a-icon-alt"]//text()')
-        name  = ' '.join(' '.join(raw_name).split())
-        image = ' '.join(' '.join(raw_image).split())
-        data = {
-                    'name': name,
-                    'price':raw_price[0][1::],
-                    'image':image,
-                    'url' : "https://www.amazon.in" + raw_url[0],
-                    'website':"amazon"
-        }
-        scraped_products.append(data)
-
-        if scraped_products:
-	        with open('amazon-scraped-data.csv','wb') as csvfile:
-	        	fieldnames = ["name","price","image","url","website"]
-	        	writer = csv.DictWriter(csvfile,fieldnames = fieldnames,quoting=csv.QUOTE_ALL)
-	        	writer.writeheader()
-	        	for data in scraped_products:
-	        		writer.writerow(data)
-    	else:
-        	print("No data scraped")
-
-	print scraped_products
-
-	return render_template("recommended_products.html",scraped_products = scraped_products)
 
 if __name__ == '__main__':
    app.run(debug = True)
 
-'''
 
+
+'''
 	kids = 0-5
 	children = 6-12
 	teenager = 13-19
 	adult = 20-31
 	elderly = 32 +
-  
+
 '''
